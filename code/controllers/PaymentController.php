82bc46ca5accdb06103ae57fcf9d69a127492b43
Cosmo Mao <gongyuan.mao@cosmocommerce.com>
2014-01-10 18:03:12 +0800

    init

---
+++ code/controllers/PaymentController.php
@@ -0,0 +1,233 @@
+<?php	 	 	 	 	 	 	 	 
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category	CosmoCommerce
+ * @package 	CosmoCommerce_Tenpay
+ * @copyright	Copyright (c) 2009 CosmoCommerce,LLC. (http://www.cosmocommerce.com)
+ * @contact :
+ * T: +86-021-66346672
+ * L: Shanghai,China
+ * M:sales@cosmocommerce.com
+ */
+class CosmoCommerce_CosmoTenpay_PaymentController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Order instance
+     */
+    protected $_order;
+
+    /**
+     *  Get order
+     *
+     *  @param    none
+     *  @return	  Mage_Sales_Model_Order
+     */
+    public function getOrder()
+    {
+        if ($this->_order == null) {
+            $session = Mage::getSingleton('checkout/session');
+            $this->_order = Mage::getModel('sales/order');
+            $this->_order->loadByIncrementId($session->getLastRealOrderId());
+        }
+        return $this->_order;
+    }
+
+    /**
+     * When a customer chooses CosmoTenpay on Checkout/Payment page
+     *
+     */
+	public function redirectAction()
+	{
+		$session = Mage::getSingleton('checkout/session');
+		$session->setCosmoTenpayPaymentQuoteId($session->getQuoteId());
+
+		$order = $this->getOrder();
+
+		if (!$order->getId()) {
+			$this->norouteAction();
+			return;
+		}
+
+		$order->addStatusToHistory(
+			$order->getStatus(),
+			Mage::helper('cosmotenpay')->__('Customer was redirected to Tenpay')
+		);
+		$order->save();
+
+		$this->getResponse()
+			->setBody($this->getLayout()
+				->createBlock('cosmotenpay/redirect')
+				->setOrder($order)
+				->toHtml());
+
+        $session->unsQuoteId();
+    }
+
+	/**
+	 *  CosmoTenpay response router
+	 *
+	 *  @param    none
+	 *  @return	  void
+	 */
+	public function notifyAction()
+	{
+		$model = Mage::getModel('cosmotenpay/payment');
+        
+        if ($this->getRequest()->isPost()) {
+			$postData = $this->getRequest()->getPost();
+        	$method = 'post';
+
+		} else if ($this->getRequest()->isGet()) {
+			$postData = $this->getRequest()->getQuery();
+			$method = 'get';
+
+		} else {
+			$model->generateErrorResponse();
+		}
+
+
+
+		$order = Mage::getModel('sales/order')
+			->loadByIncrementId($postData['reference']);
+
+		if (!$order->getId()) {
+			$model->generateErrorResponse();
+		}
+
+		if ($returnedMAC == $correctMAC) {
+			if (1) {
+				$order->addStatusToHistory(
+					$model->getConfigData('order_status_payment_accepted'),
+					Mage::helper('cosmotenpay')->__('Payment accepted by Tenpay')
+				);
+				
+				$order->sendNewOrderEmail();
+
+				if ($this->saveInvoice($order)) {
+//                $order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true);
+				}
+				
+			 } else {
+			 	$order->addStatusToHistory(
+					$model->getConfigData('order_status_payment_refused'),
+					Mage::helper('cosmotenpay')->__('Payment refused by Tenpay')
+				);
+				
+				// TODO: customer notification on payment failure
+			 }
+				
+			$order->save();
+
+        } else {
+            $order->addStatusToHistory(
+                Mage_Sales_Model_Order::STATE_CANCELED,//$order->getStatus(),
+                Mage::helper('cosmotenpay')->__('Returned MAC is invalid. Order cancelled.')
+            );
+            $order->cancel();
+            $order->save();
+            $model->generateErrorResponse();
+        }
+    }
+
+    /**
+     *  Save invoice for order
+     *
+     *  @param    Mage_Sales_Model_Order $order
+     *  @return	  boolean Can save invoice or not
+     */
+    protected function saveInvoice(Mage_Sales_Model_Order $order)
+    {
+        if ($order->canInvoice()) {
+            $convertor = Mage::getModel('sales/convert_order');
+            $invoice = $convertor->toInvoice($order);
+            foreach ($order->getAllItems() as $orderItem) {
+               if (!$orderItem->getQtyToInvoice()) {
+                   continue;
+               }
+               $item = $convertor->itemToInvoiceItem($orderItem);
+               $item->setQty($orderItem->getQtyToInvoice());
+               $invoice->addItem($item);
+            }
+            $invoice->collectTotals();
+            $invoice->register()->capture();
+            Mage::getModel('core/resource_transaction')
+               ->addObject($invoice)
+               ->addObject($invoice->getOrder())
+               ->save();
+            return true;
+        }
+
+        return false;
+    }
+
+	/**
+	 *  Success payment page
+	 *
+	 *  @param    none
+	 *  @return	  void
+	 */
+	public function successAction()
+	{
+		$session = Mage::getSingleton('checkout/session');
+		$session->setQuoteId($session->getCosmoTenpayPaymentQuoteId());
+		$session->unsCosmoTenpayPaymentQuoteId();
+		
+		$order = $this->getOrder();
+		
+		if (!$order->getId()) {
+			$this->norouteAction();
+			return;
+		}
+
+		$order->addStatusToHistory(
+			$order->getStatus(),
+			Mage::helper('cosmotenpay')->__('Customer successfully returned from Tenpay')
+		);
+        
+		$order->save();
+        
+		$this->_redirect('checkout/onepage/success');
+	}
+
+	/**
+	 *  Failure payment page
+	 *
+	 *  @param    none
+	 *  @return	  void
+	 */
+	public function errorAction()
+	{
+        $session = Mage::getSingleton('checkout/session');
+        $errorMsg = Mage::helper('cosmotenpay')->__(' There was an error occurred during paying process.');
+
+        $order = $this->getOrder();
+
+        if (!$order->getId()) {
+            $this->norouteAction();
+            return;
+        }
+        if ($order instanceof Mage_Sales_Model_Order && $order->getId()) {
+            $order->addStatusToHistory(
+                Mage_Sales_Model_Order::STATE_CANCELED,//$order->getStatus(),
+                Mage::helper('cosmotenpay')->__('Customer returned from Tenpay.') . $errorMsg
+            );
+            
+            $order->save();
+        }
+
+        $this->loadLayout();
+        $this->renderLayout();
+        Mage::getSingleton('checkout/session')->unsLastRealOrderId();
+    }
+}
